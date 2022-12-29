9b3e51ac1677e05fdfe96d6e3666e7bc177f0c2e
Timotej <timi.blazic@gmail.com>
2022-12-29 16:03:32 +0100

    frontend dashboard rwd fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,28 @@
                         <button class="dashboard__nav__link" data-content="myComments">My Comments</button>
                     </div>
                 </div>
+
+                <!-- ACCORDION MENU FOR MOBILE -->
+                <div class="accordion">
+                    <a class="accordion__title" href="#">Navigation</a>
+                    <div class="accordion__content">
+                        <div class="dashboard__nav__item active">
+                            <button class="dashboard__nav__link" data-content="myProfile">My Profile</button>
+                        </div>
+                        <div class="dashboard__nav__item">
+                            <button class="dashboard__nav__link" data-content="favoriteDrinks">Favorite Drinks</button>
+                        </div>
+                        <div class="dashboard__nav__item">
+                            <button class="dashboard__nav__link" data-content="myRatings">My Ratings</button>
+                        </div>
+                        <div class="dashboard__nav__item">
+                            <button class="dashboard__nav__link" data-content="myComments">My Comments</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- ACCORDION MENU FOR MOBILE -->
+
+
                 <div class="dashboard__content white-box">
                     <div class="dashboard__content__item active" id="myProfile">
                         My profile content
