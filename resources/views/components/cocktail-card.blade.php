1412f2e2a660394f26d840014a867081e9698943
Timotej <timi.blazic@gmail.com>
2023-01-03 14:25:38 +0100

    added favorite button on cocktail card

--- resources/views/components/cocktail-card.blade.php
+++ resources/views/components/cocktail-card.blade.php
@@ -6,6 +6,21 @@
         <img src="{{ asset('images/cocktails/' . $cocktail->image) }}" alt="Promo" width="100" height="150">
     </div>
     <div class="cocktail__name">
-        {{ $cocktail->name }}
+        <div class="cocktail__name__favorites">
+            <form class="drink__favorite__form favorite__form" method="POST" action="{{ config('app.url', '/') }}/drink/{{ $cocktail->id }}/favorite">
+                @csrf
+                <button class="drink__favorite__form__button favorite__form__button">
+                    @if ($cocktail->favorites()->where('user_id', auth()->user()->id)->exists())
+                        <span class="drink__favorite__form__button__heart favorite__form__button__heart active"></span>
+                    @else
+                        <span class="drink__favorite__form__button__heart favorite__form__button__heart"></span>
+                    @endif
+                </button>
+            </form>
+        </div>
+
+        <span class="cocktail__name__text">
+            {{ $cocktail->name }}
+        </span>
     </div>
 </div>
