1affb0dedc9806fdc728472388ea2ee7580d7a6f
Timotej <timi.blazic@gmail.com>
2023-04-03 22:23:17 +0200

    migration added user roles

--- database/migrations/2023_01_16_225620_update_users_table.php
+++ database/migrations/2023_01_16_225620_update_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Role;
 
 return new class extends Migration
 {
@@ -13,6 +14,16 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::table('roles')->truncate();
+
+        $roles = ['admin', 'general'];
+
+        foreach ($roles as $role) {
+            $model = new Role();
+            $model->name = $role;
+            $model->save();
+        }
+
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')->default(2)->constrained('roles');
         });
@@ -28,5 +39,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('role_id');
         });
+
+        DB::table('roles')->truncate();
     }
 };
