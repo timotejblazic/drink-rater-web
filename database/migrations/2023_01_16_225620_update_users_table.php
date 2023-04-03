16b9d75ebd23b3377ea4e944727564de148841c0
Timotej <timi.blazic@gmail.com>
2023-04-03 22:38:12 +0200

    migration update user table fix

--- database/migrations/2023_01_16_225620_update_users_table.php
+++ database/migrations/2023_01_16_225620_update_users_table.php
@@ -37,6 +37,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
             $table->dropColumn('role_id');
         });
 
